6d55962ede776d013b20fd3a83d3e38d34be748e
juliopavila <javila1997@gmail.com>
2024-03-11 16:26:42 -0300

    Update simulate.js

--- api/simulate.js
+++ api/simulate.js
@@ -1,7 +1,14 @@
-// api/simulate.js
-const fetch = require("node-fetch");
-
 module.exports = async (req, res) => {
+  let fetch;
+
+  // Import dinámico de node-fetch
+  try {
+    fetch = (await import('node-fetch')).default;
+  } catch (error) {
+    console.error('Error importing node-fetch:', error);
+    return res.status(500).json({ error: 'Failed to import node-fetch' });
+  }
+
   const { body } = req;
 
   try {
